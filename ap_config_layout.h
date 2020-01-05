e80a3468a7044ebd0693e95dd01014102f815aeb
ParthPali <54657955+ParthPali@users.noreply.github.com>
2020-01-04 19:07:39 -0800

    Add files via upload

---
+++ ap_config_layout.h
@@ -0,0 +1,64 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  ap_config_layout.h
+ * @brief Apache Config Layout
+ */
+
+#ifndef AP_CONFIG_LAYOUT_H
+#define AP_CONFIG_LAYOUT_H
+
+/* Configured Apache directory layout */
+#define DEFAULT_PREFIX "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI"
+#define DEFAULT_EXP_EXEC_PREFIX "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI"
+#define DEFAULT_REL_EXEC_PREFIX ""
+#define DEFAULT_EXP_BINDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/bin"
+#define DEFAULT_REL_BINDIR "bin"
+#define DEFAULT_EXP_SBINDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/bin"
+#define DEFAULT_REL_SBINDIR "bin"
+#define DEFAULT_EXP_LIBEXECDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/modules"
+#define DEFAULT_REL_LIBEXECDIR "modules"
+#define DEFAULT_EXP_MANDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/man"
+#define DEFAULT_REL_MANDIR "man"
+#define DEFAULT_EXP_SYSCONFDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/conf"
+#define DEFAULT_REL_SYSCONFDIR "conf"
+#define DEFAULT_EXP_DATADIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI"
+#define DEFAULT_REL_DATADIR ""
+#define DEFAULT_EXP_INSTALLBUILDDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/build"
+#define DEFAULT_REL_INSTALLBUILDDIR "build"
+#define DEFAULT_EXP_ERRORDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/error"
+#define DEFAULT_REL_ERRORDIR "error"
+#define DEFAULT_EXP_ICONSDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/icons"
+#define DEFAULT_REL_ICONSDIR "icons"
+#define DEFAULT_EXP_HTDOCSDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/htdocs"
+#define DEFAULT_REL_HTDOCSDIR "htdocs"
+#define DEFAULT_EXP_MANUALDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/manual"
+#define DEFAULT_REL_MANUALDIR "manual"
+#define DEFAULT_EXP_CGIDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/cgi-bin"
+#define DEFAULT_REL_CGIDIR "cgi-bin"
+#define DEFAULT_EXP_INCLUDEDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/include"
+#define DEFAULT_REL_INCLUDEDIR "include"
+#define DEFAULT_EXP_LOCALSTATEDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI"
+#define DEFAULT_REL_LOCALSTATEDIR ""
+#define DEFAULT_EXP_RUNTIMEDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/logs"
+#define DEFAULT_REL_RUNTIMEDIR "logs"
+#define DEFAULT_EXP_LOGFILEDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/logs"
+#define DEFAULT_REL_LOGFILEDIR "logs"
+#define DEFAULT_EXP_PROXYCACHEDIR "/Users/Paliwal/Documents/GoogleCodein/HTTPD/myApache-GCI/proxy"
+#define DEFAULT_REL_PROXYCACHEDIR "proxy"
+
+#endif /* AP_CONFIG_LAYOUT_H */
